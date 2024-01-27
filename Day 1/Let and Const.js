ab3bd7d1c581e60eaff7112f4d29e360d7c6fdf7
Sudhan M <98807138+sudh4n@users.noreply.github.com>
2024-01-27 15:12:58 +0530

    Let and Const with JavaScript

--- Day 1/Let and Const.js
+++ Day 1/Let and Const.js
@@ -61,17 +61,19 @@ function readLine() {
 
 function main() {
     // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r, area, perimeter;
     const PI = Math.PI;
-    let r = parseFloat(readLine());
+    
+    r = readLine();
     
     // Print the area of the circle:
-    let area = PI * Math.pow(r,2);
+    area = PI * r * r;
     console.log(area);
     
     // Print the perimeter of the circle:
-    var perimeter = 2 * PI * r;
+    perimeter = 2 * PI * r;
     console.log(perimeter);
-}
+
 
     try {    
         // Attempt to redefine the value of constant variable PI
